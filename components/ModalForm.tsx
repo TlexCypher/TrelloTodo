ce20219fe5c66fabc04ff839d61fe2ff654f2ba9
Taichi Araki <araki@fun.bio.keio.ac.jp>
2024-03-04 00:44:53 +0900

    add task functionality end.

--- components/ModalForm.tsx
+++ components/ModalForm.tsx
@@ -1,26 +1,44 @@
+import useBoardStore from '@/store/BoardStore';
 import useModalStore from '@/store/ModalStore'
 import { RadioGroup } from '@headlessui/react'
-import React from 'react'
+import React, { ChangeEvent } from 'react'
 
 const ModalForm = () => {
-  const [taskType, setTaskType] = useModalStore((state) => [
+  const [taskType, setTaskType, closeModal] = useModalStore((state) => [
     state.taskType,
     state.setTaskType,
+    state.closeModal,
+  ]);
+
+  const [newTaskInput, setNewTaskInput, addTask] = useBoardStore((state) => [
+    state.newTaskInput,
+    state.setNewTaskInput,
+    state.addTask,
   ])
 
-  const handleSubmitAddTask = () => {
+  const handleNewTaskInput = (e: ChangeEvent<HTMLInputElement>) => {
+    setNewTaskInput(e.target.value);
+  }
+
+  const handleSubmitAddTask = (e) => {
+    addTask(taskType, newTaskInput);
+    closeModal();
+    e.preventDefault();
   }
 
   return (
-    <div className='max-w-md mx-auto w-full bg-white px-20 py-10 text-center rounded-xl'>
+    <div
+      className='max-w-md mx-auto w-full bg-white px-20 py-10 text-center rounded-xl'
+    >
       <p className='mb-5 font-bold text-2xl text-pink-600/80'>Add Task Form</p>
       <input
         placeholder={"Enter task title"}
         className='flex justify-center items-center w-full outline-none h-16 border rounded-lg px-3 mb-5'
+        onChange={handleNewTaskInput}
       />
       <p className='mb-3 font-bold drop-shadow-sm'>Select task type</p>
-      <RadioGroup value={taskType}>
-        <RadioGroup.Option value="To DO" className={"mb-5"}>
+      <RadioGroup value={taskType} onChange={setTaskType}>
+        <RadioGroup.Option value="To Do" className={"mb-5"}>
           {({ active }) => (
             <div className={active ? 'bg-red-400/40 py-6 rounded-lg text-white text-lg mx-auto' :
               'bg-red-400 py-6 rounded-lg text-white text-lg mx-auto'}
@@ -48,13 +66,13 @@ const ModalForm = () => {
           )}
         </RadioGroup.Option>
       </RadioGroup>
-      <button className='bg-pink-300/60 text-pink-600/80 italic font-bold p-2 rounded-lg 
-        hover:ring ring-pink-300 hover:ring-pink-500 transition duration-300'
-        onSubmit={handleSubmitAddTask}
+      <button type="submit" className='bg-pink-300/60 text-pink-600/80 italic font-bold p-2 rounded-lg
+      hover:ring ring-pink-300 hover:ring-pink-500 transition duration-300'
+        onClick={e => handleSubmitAddTask(e)}
       >
         Let's Go!
       </button>
-    </div>
+    </div >
   )
 }
 
--- store/BoardStore.ts
+++ store/BoardStore.ts
@@ -1,15 +1,19 @@
-import { databases } from "@/appWrite";
+import { ID, databases } from "@/appWrite";
+import Board from "@/components/Board";
 import getColumnsGroupedByTypedColumn from "@/lib/getColumnGroupedByTypedColumn";
 import { create } from "zustand";
 
 interface BoardState {
   board: Board;
   searchString: string;
+  newTaskInput: string,
   setBoard: (board: Board) => void;
   setSearchString: (searchString: string) => void;
+  setNewTaskInput: (newTaskInput: string) => void;
   getBoard: () => void;
   updateDB: (todo: Todo, type: TypedColumn) => void;
   deleteTask: (todoId: string, typedColumn: TypedColumn, taskIndex: number) => void;
+  addTask: (taskType: string, taskContent: string) => void;
 }
 
 const useBoardStore = create<BoardState>((set, get) => ({
@@ -17,6 +21,7 @@ const useBoardStore = create<BoardState>((set, get) => ({
     columns: new Map<TypedColumn, Column>(),
   },
   searchString: "",
+  newTaskInput: "",
   setBoard: (board: Board) => set({ board }),
   getBoard: async () => {
     const board = await getColumnsGroupedByTypedColumn();
@@ -24,6 +29,9 @@ const useBoardStore = create<BoardState>((set, get) => ({
   },
   setSearchString: (searchString: string) => {
     set({ searchString });
+  },
+  setNewTaskInput: (newTaskInput: string) => {
+    set({ newTaskInput });
   },
   updateDB: (todo: Todo, type: TypedColumn) => {
     databases.updateDocument(
@@ -48,6 +56,40 @@ const useBoardStore = create<BoardState>((set, get) => ({
       await databases.deleteDocument(process.env.NEXT_PUBLIC_DB_ID!, process.env.NEXT_PUBLIC_COLLECTION_ID!, todoId);
     }
     deleteTaskFunc();
+  },
+  addTask: (taskType: string, taskContent: string) => {
+    const addTaskFunc = async () => {
+      let convertedTaskType: TypedColumn = "todo";
+      if (taskType === "In Progress") convertedTaskType = "inprogress";
+      else if (taskType === "Done") convertedTaskType = "done";
+
+      const resp = await databases.createDocument(
+        process.env.NEXT_PUBLIC_DB_ID!,
+        process.env.NEXT_PUBLIC_COLLECTION_ID!,
+        ID.unique(),
+        {
+          type: convertedTaskType,
+          content: taskContent,
+        }
+      )
+      set({ newTaskInput: "" })
+
+      /* Make new Board and set({board: newBoard}) */
+      const newColumns = new Map(get().board.columns);
+      newColumns.get(resp.type)?.todos.push({
+        $id: resp.$id,
+        $createdAt: resp.$collectionId,
+        type: resp.type,
+        content: resp.content,
+      })
+
+      set({
+        board: {
+          columns: newColumns
+        }
+      })
+    };
+    addTaskFunc();
   },
 }));
 
